0a3cd1d5b2094705077aa6928d496c2730f21580
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-08-27 10:36:48 -0500

    remove lambda and use def

--- lib/queries.py
+++ lib/queries.py
@@ -1,11 +1,28 @@
 
 comma = ','.join
-quest = lambda l: '=?,'.join(l) + '=?'
 
-exists_table = "SELECT name FROM sqlite_master WHERE type='table' AND name='assets'"
+exists_table = "SELECT name FROM sqlite_master "
+exists_table += "WHERE type='table' AND name='assets'"
 
-read_all = lambda keys: 'select ' + comma(keys) + ' from assets order by play_order'
-read = lambda keys: 'select ' + comma(keys) + ' from assets where asset_id=?'
-create = lambda keys: 'insert into assets (' + comma(keys) + ') values (' + comma(['?'] * len(keys)) + ')'
 remove = 'delete from assets where asset_id=?'
-update = lambda keys: 'update assets set ' + quest(keys) + ' where asset_id=?'
+
+
+def quest(iterable):
+    return '=?,'.join(iterable) + '=?'
+
+
+def read_all(keys):
+    return 'select {} from assets order by play_order'.format(comma(keys))
+
+
+def read(keys):
+    return "select {} from assets where asset_id=?".format(comma(keys))
+
+
+def create(keys):
+    tmpl = 'insert into assets ({}) values ({})'
+    return tmpl.format(comma(keys), comma(['?'] * len(keys)))
+
+
+def update(keys):
+    return "update assets set {} where asset_id=?".format(quest(keys))
